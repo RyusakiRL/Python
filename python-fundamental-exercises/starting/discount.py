8be5c5ae5e3ab937a45cef3937eb17882cd7a8a5
Caio Kazu <caioitaya08@gmail.com>
2026-01-28 19:18:35 -0400

    New salary with increse of 15%

---
+++ python-fundamental-exercises/starting/discount.py
@@ -0,0 +1,3 @@
+discount = float(input("Write the price of product in R$"));
+
+print(f"The price of product with discount is R${discount*0.95}");
---
+++ python-fundamental-exercises/starting/newsalary.py
@@ -0,0 +1,3 @@
+salary = float(input("Write your salary in R$"));
+
+print(f"Your new salary with the increase is R${salary*1.15}");
