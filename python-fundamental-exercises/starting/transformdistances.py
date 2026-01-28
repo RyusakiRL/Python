302e0ee65d725ffbaa132295b78313afdd2c5869
Caio Kazu <caioitaya08@gmail.com>
2026-01-28 11:57:53 -0400

    Measurement conversion

---
+++ python-fundamental-exercises/starting/transformdistances.py
@@ -0,0 +1,10 @@
+km = 1/1000;
+hm = 1/100;
+dam = 1/10;
+dm = 10;
+cm = 100;
+mm = 1000;
+
+meters = int(input("Write the distance number in meters\n"));
+
+print(f"The correspondent distance is:\n{meters*km}km\n{meters*hm}hm\n{meters*dam}dam\n{meters*dm}\n{meters*cm}cm\n{meters*mm}mm");
