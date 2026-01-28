759287d94602fe9aadf743f23a93d7d60b8456ba
Caio Kazu <caioitaya08@gmail.com>
2026-01-28 19:04:10 -0400

    Quantity of paint necessary to paint a wall

---
+++ python-fundamental-exercises/starting/areacalculator.py
@@ -0,0 +1,7 @@
+large = float(input("Write the larger of the wall\n"));
+height = float(input("Write the height of the wall\n"));
+
+area = large*height;
+ink = 2;
+
+print(f"The bucket of ink necessary to paint the wall is {area/2} liters");
