e7dcbd93306be48c315dd0158c41b006ab944651
Caio Kazu <caioitaya08@gmail.com>
2026-01-28 19:11:54 -0400

    Calculator of delta

---
+++ python-fundamental-exercises/starting/deltavalue.py
@@ -0,0 +1,5 @@
+a = float(input("Enter the value of a\n"));
+b = float(input("Enter the value of b\n"));
+c = float(input("Enter the value of c\n"));
+
+print(f"The value of delta is {(b*b)-4*a*c}");
