1cfc35682c71bdb59b27ca01639f9e128a9b15a6
Caio Kazu <caioitaya08@gmail.com>
2026-02-03 16:34:14 -0400

    Existence validation of triangle

--- python-fundamental-exercises/basiconditions/existencecondition.py
+++ python-fundamental-exercises/basiconditions/existencecondition.py
@@ -1,8 +1,8 @@
-a = float(input("Write the first side of triangle"));
-b = float(input("Write the second side of triangle"));
-c = float(input("Write the third side of triangle"));
+a = float(input("Write the first side of triangle\n"));
+b = float(input("Write the second side of triangle\n"));
+c = float(input("Write the third side of triangle\n"));
 
-if a<b+c & b<a+c & c<a+b:
+if a<b+c and b<a+c and c<a+b:
     print("The triangle exists");
 else:
     print("The triangle don't exist");
