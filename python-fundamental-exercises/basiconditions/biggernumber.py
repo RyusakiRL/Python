a6ba035f6d02896665265292779fc920b38f5e32
Caio Kazu <caioitaya08@gmail.com>
2026-02-03 17:00:00 -0400

    The bigger number between two

---
+++ python-fundamental-exercises/basiconditions/biggernumber.py
@@ -0,0 +1,7 @@
+number1 = int(input("Write the first number\n"));
+number2 = int(input("Write the second number\n"));
+
+if number1>number2:
+    print(f"The number {number1} is bigger");
+else:
+    print(f"The number {number2} is bigger");
