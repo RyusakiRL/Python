8f2fb7f53ca76e87c9245ae5fb3570c8ee28c4c7
Caio Kazu <caioitaya08@gmail.com>
2026-02-05 15:26:21 -0400

    Money gained based in hours of exercise

--- python-fundamental-exercises/basiconditions/lifepoints.py
+++ python-fundamental-exercises/basiconditions/lifepoints.py
@@ -2,8 +2,8 @@ exercise = int(input("How much time in hours, you do in exercise this month\n"))
 
 match exercise:
     case _ if exercise<=10:
-        print(f"You trained {exercise}, gaining ${exercise*2*0.05}");
+        print(f"You trained {exercise} hours, gaining ${exercise*2*0.05}");
     case _ if exercise>10 and exercise<=20:
-        print(f"You trained {exercise}, gaining ${exercise*5*0.05}");
+        print(f"You trained {exercise} hours, gaining ${exercise*5*0.05}");
     case _ if exercise>20:
-        print(f"You trained {exercise}, gaining ${exercise*10*0.05}");
+        print(f"You trained {exercise} hours, gaining ${exercise*10*0.05}");
