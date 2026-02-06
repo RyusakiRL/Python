3b8c81d1ee1284996b01e92682a0b38337f13df5
Caio Kazu <caioitaya08@gmail.com>
2026-02-06 10:56:53 -0400

    Sequence with corrections, havind function back to front

---
+++ python-fundamental-exercises/repeater/limitedsequence2.py
@@ -0,0 +1,16 @@
+initnumber = int(input("Write the first number\n"));
+lastnumber = int(input("Write the last number\n"));
+increment = int(input("Write the increment\n"));
+
+while initnumber<lastnumber:
+    print(f"{initnumber}", end=" ")
+    initnumber = initnumber+increment;
+
+
+
+if lastnumber<=initnumber:
+    while lastnumber<=initnumber:
+        print(f"{initnumber}", end=" ")
+        initnumber = initnumber-increment;
+
+print("End!");
