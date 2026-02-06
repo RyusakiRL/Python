687002d9f5e2c82f54914367846644d93ff6fad1
Caio Kazu <caioitaya08@gmail.com>
2026-02-05 21:15:03 -0400

    Reader of a determined number to finalize the sequence

---
+++ python-fundamental-exercises/repeater/sequencial.py
@@ -0,0 +1,8 @@
+number = 0;
+numberfinal = int(input("Write a integer and positive number\n"));
+
+while number<=numberfinal:
+    print(f"{number}", end=" ")
+    number = number+1;
+
+print("End!");
