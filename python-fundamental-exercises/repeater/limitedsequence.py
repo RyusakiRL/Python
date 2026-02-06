86e4d92101a09b1272174d903492e91635741ec2
Caio Kazu <caioitaya08@gmail.com>
2026-02-06 01:41:44 -0400

    Sequence of numbers, based in increment, initial number, last number

---
+++ python-fundamental-exercises/repeater/limitedsequence.py
@@ -0,0 +1,9 @@
+initnumber = int(input("Write the first number\n"));
+lastnumber = int(input("Write the last number\n"));
+increment = int(input("Write the increment\n"));
+
+while initnumber<=lastnumber:
+    print(f"{initnumber}", end=" ")
+    initnumber = initnumber+increment;
+
+print("End!");
