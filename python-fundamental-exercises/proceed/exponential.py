2d8e57a09ef9fcf446b275ddd76ec6deffab57b4
Caio Kazu <caioitaya08@gmail.com>
2026-02-10 23:45:44 -0400

    Reader of exponential using function

---
+++ python-fundamental-exercises/proceed/exponential.py
@@ -0,0 +1,9 @@
+def exponential(numberone, numbertwo):
+    print(numberone**numbertwo);
+
+
+number1 = int(input("Insert the base number\n"))
+number2 = int(input("Insert the exponent\n"))
+
+print("--------RESULTS--------")
+exponential(number1, number2)
