c016e1aa5a223b0fc9543321c78e334a22b65104
Caio Kazu <caioitaya08@gmail.com>
2026-02-10 22:25:47 -0400

    Test with numbers and repeaters in function

---
+++ python-fundamental-exercises/proceed/moreadd.py
@@ -0,0 +1,8 @@
+def generator(texto, number):
+    print("+-------=======------+")
+    for i in range(number):
+        print(texto)
+    print("+-------=======------+");
+
+
+generator("Hello", 4)
