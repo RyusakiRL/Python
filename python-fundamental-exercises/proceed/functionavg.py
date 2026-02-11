61a2d6870c2213c881e03485fa54b81531f0b3ee
Caio Kazu <caioitaya08@gmail.com>
2026-02-10 23:36:06 -0400

    Function average

---
+++ python-fundamental-exercises/proceed/functionavg.py
@@ -0,0 +1,9 @@
+def avg(first, second):
+    print("The result is:\n")
+    print((first+second)/2);
+
+
+number1 = int(input("Enter the first note\n"))
+number2 = int(input("Enter the second note\n"))
+
+avg(number1, number2)
