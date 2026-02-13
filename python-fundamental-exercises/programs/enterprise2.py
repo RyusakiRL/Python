26b7f4722c1d41f56de8e039dc0b1192bc154442
Caio Kazu <caioitaya08@gmail.com>
2026-02-13 15:30:43 -0400

    Shop stock with atualization of products

--- python-fundamental-exercises/programs/enterprise2.py
+++ python-fundamental-exercises/programs/enterprise2.py
@@ -1,29 +1,47 @@
 shop_stock = [
-    {"Mark": "Tagima", "model": "Stratocaster", "price": 832.99, "quantity": 15},
-    {"Mark": "Konig", "model": "Acoustic", "price": 500.00, "quantity": 12},
-    {"Mark": "Yamaha", "model": "Pacifist", "price": 1320.00, "quantity": 5}
+    {"Mark": "Tagima", "price": 832.99, "quantity": 15},
+    {"Mark": "Konig", "price": 500.00, "quantity": 12},
+    {"Mark": "Yamaha", "price": 1320.00, "quantity": 5}
 ]
 
-def stock_list(stock_list, marksearch):
+def stock_list(sr, stock_list, marksearch, newquantity, newprice):
+    if sr == "register" or sr == "cadaster":
+       new_instrument = {
+           "Mark":  marksearch,
+           "price": newprice,
+           "quantity": newquantity
+       }
 
-    for instrument in stock_list:
+       shop_stock.append(new_instrument)
+       print(f"Sucess! {marksearch} added to stock.")
 
-        if instrument["Mark"] == marksearch:
+    if sr == "sell":
+        for instrument in stock_list:
 
-            if instrument["quantity"]>0:
-                instrument["quantity"]-=1
-                print(f"Sale realized!! Value:{instrument['price']}")
-                print(f"The new stock of {marksearch}: {instrument['quantity']}")
-            else:
-                print(f"The {marksearch} esgoted!!")
+            if instrument["Mark"] == marksearch:
 
-            return
+                if instrument["quantity"]>0:
+                    instrument["quantity"]-=1
+                    print(f"Sale realized!! Value:{instrument['price']}")
+                    print(f"The new stock of {marksearch}: {instrument['quantity']}")
+                else:
+                    print(f"The {marksearch} esgoted!!")
+
+                return
     
-    print("Instrument not encountered");
+        print("Instrument not encountered");
 
 
 while True:
     sell = input("You want sell or cadaster the new product?\n")
     if sell == "sell":
         mark = input("Insert the name of mark\n")
-        stock_list(shop_stock, mark )
+
+        stock_list(sell, shop_stock, mark, 0, 0 )
+    else:
+        mark = input("Insert the name of mark\n")
+        priceu = int(input("Insert the price\n"))
+        quant = int(input("Insert the quantity\n"))
+        stock_list(sell, shop_stock, mark, quant, priceu)
+
+        
