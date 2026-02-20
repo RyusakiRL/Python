e5747729554c9fc1a24f19bb49b464f89fb3278d
Caio Kazu <caioitaya08@gmail.com>
2026-02-20 09:57:30 -0400

    Updating the file by adding class methods.

---
+++ python-fundamental-exercises/programs/shop_poo2.py
@@ -0,0 +1,27 @@
+class Instrument:
+    def __init__(self, mark, price, quantity):
+        self.mark = mark
+        self.price = price
+        self.quantity = quantity
+   
+    def sell(self, sell_quantity):    
+
+        if self.quantity>=sell_quantity:
+            self.quantity-=sell_quantity
+
+            print(f"Sucess! {sell_quantity} {self.mark}(s) sold.")
+        else:
+            print("Stock insufficient!")
+product1 = Instrument("Yamaha", 1750, 12)
+product2 = Instrument("Tagima", 1100, 10)
+
+act = input("Type: 'sell'")
+
+if act == "sell":
+    try:
+        quantities = int(input("Insert the sell quantity:\n "))
+        product1.sell(quantities)
+        print(f"Atual stock of {product1.mark}: {product1.quantity}")
+
+    except ValueError:
+        print("Enter a number!!")
