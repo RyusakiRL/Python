7f5610c0f13940099950301fba13b51e52038a12
Caio Kazu <caioitaya08@gmail.com>
2026-02-20 15:14:24 -0400

    Stock using POO

--- python-fundamental-exercises/programs/shop_poo2.py
+++ python-fundamental-exercises/programs/shop_poo2.py
@@ -12,16 +12,29 @@ class Instrument:
             print(f"Sucess! {sell_quantity} {self.mark}(s) sold.")
         else:
             print("Stock insufficient!")
+    
+    def adjust(self, adjust_price):
+        self.price = self.price*adjust_price
+
 product1 = Instrument("Yamaha", 1750, 12)
 product2 = Instrument("Tagima", 1100, 10)
 
-act = input("Type: 'sell'")
+act = input("Type: 'sell' or 'adjust': ")
 
 if act == "sell":
     try:
         quantities = int(input("Insert the sell quantity:\n "))
         product1.sell(quantities)
         print(f"Atual stock of {product1.mark}: {product1.quantity}")
+
+    except ValueError:
+        print("Enter a number!!")
+
+elif act == "adjust":
+    try:
+        adjusting = int(input("Insert the adjust:\n "))
+        product1.adjust(adjusting)
+        print(f"Atual stock of {product1.mark}: R${product1.price}")
 
     except ValueError:
         print("Enter a number!!")
