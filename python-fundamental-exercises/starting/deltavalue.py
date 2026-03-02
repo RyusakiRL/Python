5d45875fee700a5b7864738bd2526b6f97ad0556
Caio Kazu <caioitaya08@gmail.com>
2026-03-02 15:30:14 -0400

    Atividade de criar um sistema usando POO, simulando registros de livros e usuarios de uma biblioteca

--- python-fundamental-exercises/starting/deltavalue.py
+++ python-fundamental-exercises/starting/deltavalue.py
@@ -2,4 +2,6 @@ a = float(input("Enter the value of a\n"));
 b = float(input("Enter the value of b\n"));
 c = float(input("Enter the value of c\n"));
 
-print(f"The value of delta is {(b*b)-4*a*c}");
+delta = (b*b) -4*a*c;
+
+print(f"The value of delta is {delta}");
