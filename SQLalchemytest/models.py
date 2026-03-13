1fabc3bdbbff1727ae418e8dd26e1f3aa6078b3c
Caio Kazu <caioitaya08@gmail.com>
2026-03-13 15:32:23 -0400

    Criacao da tabela SQLAlchemy

--- SQLalchemytest/models.py
+++ SQLalchemytest/models.py
@@ -1,11 +1,23 @@
-"""Modelos de classe do banco de dados"""
+"""Cria as tabelas do SQLite usando SQLAlchemy."""
 
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy.orm import declarative_base
+from database import engine
+
+Base = declarative_base()
 
 
-class ProdutoBase(BaseModel):
+class ProdutoSQL(Base):
     """Cria os produtos no SQLite"""
 
-    nome: str
-    marca: str
-    preco: float
+    __tablename__ = "produtos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False, index=True)
+    marca = Column(String, nullable=False)
+    preco = Column(Float, nullable=False)
+    quantidade = Column(Integer, default=0)
+    ativo = Column(Boolean, default=True)
+
+
+Base.metadata.create_all(bind=engine)
